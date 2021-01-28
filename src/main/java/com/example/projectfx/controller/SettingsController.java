95501bace17ece182ca8972e9668dbdd95e008b7
Adam Orłowski <feruupl@gmail.com>
2021-01-28 11:07:36 +0100

    Created settings view

---
+++ src/main/java/com/example/projectfx/controller/SettingsController.java
@@ -0,0 +1,9 @@
+package com.example.projectfx.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SettingsController {
+}
